8a2b558b2cd4a628ad08566fb3b23f73443e4693
H_Kitagawa <k.haruki.zephyranthes@gmail.com>
2025-01-14 12:29:09 +0900

    [update]　画像のズレ修正

--- src/pages/CookProcess.jsx
+++ src/pages/CookProcess.jsx
@@ -120,25 +120,9 @@ function CookProcess() {
 
                                 // 画像表示処理
                                 return ( 
-                                    <>
-                                        <div id={`balloon-${index}`} className="balloon" style={{display: `none`}}>{targetName}</div>
-                                        <div key={`menuImage-${index}`} className='imageWrapper'>
-                                            <img src={targetPath} className='gridItem' alt="献立画像" onClick={() => {
-                                                // const target = `balloon-${index}`;
-                                                // console.log(target);
-                                                
-                                                // // 吹き出し表示
-                                                // target.style.display = "block";
-
-                                                // // 10秒後非表示
-                                                // setTimeout(() => {
-                                                //     target.style.display = "none";
-                                                // }, 10000)
-
-                                            }}></img>
-                                        </div>
-                                    </>
-                                    
+                                    <div key={`menuImage-${index}`} className='imageWrapper'>
+                                        <img src={targetPath} className='gridItem' alt="献立画像"></img>
+                                    </div>
                                 )
                             }
                         )}
