36c4002f0e5c2e4b4f19ac2bee85c4b181ec60a9
ruirui14 <2230103@ecc.ac.jp>
2025-12-05 10:05:14 +0900

    ダイアログ内料理画像・確認画面表示用エンドポイント変更

--- src/pages/CustomDialog.jsx
+++ src/pages/CustomDialog.jsx
@@ -17,40 +17,74 @@ export default function CustomDialog({
   //onConfirm,
   onCancel,
 }) {
+  console.log("test_content >>>", test_content);
   return (
     <React.Fragment>
-      
       {/* Dialogコンポーネントの利用 */}
       <Dialog
         open={isOpen}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
         sx={{
-          left: "0%",   // ダイアログの表示位置のカスタマイズ
-          "& .MuiPaper-root": { // ダイアログ全体のスタイルを変更
-          backgroundColor: "#F8F5F2",
-          borderRadius: "25px", // 角丸
+          left: "0%", // ダイアログの表示位置のカスタマイズ
+          "& .MuiPaper-root": {
+            // ダイアログ全体のスタイルを変更
+            backgroundColor: "#F8F5F2",
+            borderRadius: "25px", // 角丸
           },
         }}
       >
         {/* DialogContentコンポーネントの利用 */}
         <DialogContent>
-
           {/* DialogContentTextコンポーネントの利用 */}
           <DialogContentText id="alert-dialog-description">
             {content} {/*選択中レシピ(ダイアログタイトル)*/}
           </DialogContentText>
-
         </DialogContent>
-       
-          <div className="dialogContainer">
-            <img src={test_content[0] ? `https://makeck.mattuu.com/images/${test_content[0]}.jpg` : images.selectStapleFood} width={125} height={95} alt="選択中レシピNO主食" />
-            {/* <img src="https://makeck.mattuu.com/images/00e60535e7e545c6a43b3a0baafb9200.jpg" width={125} alt="" /> */}
-            <img src={test_content[1] ? `https://makeck.mattuu.com/images/${test_content[1]}.jpg` : images.selectMainDish} width={125} height={95} alt="選択中レシピNO主菜" />
-            <img src={test_content[2] ? `https://makeck.mattuu.com/images/${test_content[2]}.jpg` : images.selectSideDish} width={125} height={95} alt="選択中レシピNO副菜" />
-            <img src={test_content[3] ? `https://makeck.mattuu.com/images/${test_content[3]}.jpg` : images.selectSoup} width={125} height={95} alt="選択中レシピNO汁物" />
-          </div>
-        
+
+        <div className="dialogContainer">
+          <img
+            src={
+              test_content[0]
+                ? `https://makeck.mattuu.com/recipe/images/${test_content[0]}.jpg`
+                : images.selectStapleFood
+            }
+            width={125}
+            height={95}
+            alt="選択中レシピNO主食"
+          />
+          {/* <img src="https://makeck.mattuu.com/images/00e60535e7e545c6a43b3a0baafb9200.jpg" width={125} alt="" /> */}
+          <img
+            src={
+              test_content[1]
+                ? `https://makeck.mattuu.com/recipe/images/${test_content[1]}.jpg`
+                : images.selectMainDish
+            }
+            width={125}
+            height={95}
+            alt="選択中レシピNO主菜"
+          />
+          <img
+            src={
+              test_content[2]
+                ? `https://makeck.mattuu.com/recipe/images/${test_content[2]}.jpg`
+                : images.selectSideDish
+            }
+            width={125}
+            height={95}
+            alt="選択中レシピNO副菜"
+          />
+          <img
+            src={
+              test_content[3]
+                ? `https://makeck.mattuu.com/recipe/images/${test_content[3]}.jpg`
+                : images.selectSoup
+            }
+            width={125}
+            height={95}
+            alt="選択中レシピNO汁物"
+          />
+        </div>
 
         {/* DialogActionsコンポーネントの利用 */}
         <DialogActions>
@@ -59,20 +93,18 @@ export default function CustomDialog({
             {confirmButtonLabel}
           </Button>} */}
           {/* Buttonコンポーネントの利用 ダイアログ閉じるボタン*/}
-          <Button onClick={onCancel}
+          <Button
+            onClick={onCancel}
             sx={{
               fontSize: "20px",
               marginRight: "38%",
-                
-            }}>
+            }}
+          >
             {cancelButtonLabel}
           </Button>
         </DialogActions>
       </Dialog>
-      
     </React.Fragment>
-
-
   );
 }    
 
